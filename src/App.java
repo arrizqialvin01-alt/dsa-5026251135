bc63729052d465c244c0df1eb47ecf8854a72d19
Sayyid Maliky Alvin Arizqi <arrizqialvin01@gmail.com>
2026-09-20 22:56:58 +0700

    initial comit

---
+++ src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
+
+git config--global user.name"Sayyid Maliky Alvin Arizqi"
+git config--global user.email
+"arrizqialvin01@gmail.com"
